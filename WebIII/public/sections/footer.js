cbaf0de08ae9e4641fb23b701612304d0e11b72c
Dylan Jafet Díaz Valenciano <dydiazva@est.utn.ac.cr>
2023-11-26 23:45:45 -0600

    Login funcional

--- WebIII/public/sections/footer.js
+++ WebIII/public/sections/footer.js
@@ -3,14 +3,13 @@ class Footer extends HTMLElement{
         super();
         this.innerHTML=`<br><hr>
 <div class="div_pi">
-    <strong>Ubicados: </strong>En el aula....donde mas.<br/>
-    <strong>eMail:    </strong> <a href="mailto:jruiz@utn.ac.cr?subject=Solicito información">jruiz@utn.ac.cr</a><br />
-    <strong>Teléfono: </strong>85 42 89 18
-</div>
+    <strong>: </strong><br/>
+    <strong>:    </strong><br />
+    <strong>: </strong></div>
 <div class="div_pc">
     Carrera de Tecnologías de la información<br/>
-    Curso de Tecnologías y Sistemas Web 2<br/>
-    Desarrollado completamente a mano con malicia indigena
+    Curso de Tecnologías y Sistemas Web 3<br/>
+    Desarrollado por Eduar, William, Dylan y Keylor 
 </div>
 <div class="div_pd">
     <a href="https://www.facebook.com/"><i class="fa fa-2x fa-facebook"></i></a>
--- WebIII/public/sections/menu.js
+++ WebIII/public/sections/menu.js
@@ -2,34 +2,38 @@ class Menu extends HTMLElement{
     constructor(){
         super();
         this.innerHTML=`<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src="images/logo/northwind_T.png" alt="" width="65%"  class="d-inline-block align-text-top">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="categories.html">Categorías</a>
-                </li>
-                <li class="nav-item">
-                    <li><a class="nav-link" href="registrar.html">Registrar nuevo usuario</a></li>
-                </li>
-                <li class="nav-item">
-                <li><a class="nav-link" href="login.html">Autenticar usuario</a></li>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" onclick="salir()">Cerrar sesión</a>
-                </li>
-            </ul>
+        <div class="container-fluid">
+             <a class="navbar-brand"><img src="images/logo/libro.png" alt="" width="15%"  class="d-inline-block align-text-top"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink_02" role="button" data-bs-toggle="dropdown"
+                           aria-expanded="false">
+                            Acceder
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink_02">
+                            <li><a class="dropdown-item" href="registrar.html">Registrar nuevo usuario</a></li>
+                            <li><a class="dropdown-item" href="login.html">Autenticar usuario</a></li>
+                            <li><a class="dropdown-item" onclick="salir()">Cerrar sesión</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link active" href="proyectos.html">Proyectos</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link active" href="agregar.html">Estudiantes</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>` 
+    </nav>` 
     }     
 }
 
